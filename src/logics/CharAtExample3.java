fdc865926c48ee1ce91fe81726f9dcf8277c388a
jakkulababu <47316080+jakkulababu@users.noreply.github.com>
2019-02-04 19:59:42 +0530

    added for loop
    for loop

--- src/logics/CharAtExample3.java
+++ src/logics/CharAtExample3.java
@@ -6,10 +6,17 @@ public class CharAtExample3
 	public static void main(String[] args) 
 	{
 		String str="JavaString";
-		//int x=str.length();
+		int x=str.length();
 		for(int i=0;i<str.length();i++)
 		System.out.println(i+ "index Character :"+str.charAt(i));
 		System.out.println("Character last index is:"+str.charAt(str.length()-1));
+		
+		char array=str.ToCharArray();
+		for(int i=0;i<array.length();i++)
+		{
+			System.out.println(i);
+		}
+		
 	}
 
 }
