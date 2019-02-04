ac6da5c27d223f1fbf2d882426cb7d2eceb7f22e
jakkulababu <47316080+jakkulababu@users.noreply.github.com>
2019-02-04 19:52:51 +0530

    added new logic
    added

---
+++ src/logics/Test006.java
@@ -0,0 +1,51 @@
+package myPack1;
+
+public class Test006 {
+
+	public static void main(String[] args) 
+	{
+				{
+							for(int i=0;i<=5;i++) 
+							{
+								for(int j=5;j>i;j--) 
+								{
+									System.out.print("*");
+								}
+								for(int k=0;k<=i;k++) 
+								{
+									System.out.print("b");
+								}
+								for(int n=1;n<=i;n++) 
+								{
+									System.out.print("a");
+								}
+								for(int n1=5;n1>i;n1--) 
+								{
+									System.out.print("*");
+								}
+								System.out.println();
+							}
+							for(int i=0;i<=5;i++) 
+							{
+								for(int j=0;j<=i;j++) 
+								{
+									System.out.print("*");
+								}
+								for(int k=4;k>=i;k--) 
+								{
+									System.out.print("b");
+								}
+								for(int n=4;n>i;n--)
+								{
+									System.out.print("u");
+								}
+								for(int n1=-1;n1<i;n1++) 
+								{
+									System.out.print("*");
+								}
+								System.out.println();
+							}
+						}
+
+	}
+}
